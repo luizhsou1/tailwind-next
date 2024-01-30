9ff3f49de17b940641bb9e86ea792de824538ab4
Luiz Henrique <luiz.henrique@zenvia.com>
2024-01-30 12:50:41 -0300

    Variantes de Botões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import { SettingsTabs } from '@/components/SettingsTabs'
 import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { Select, SelectItem } from '@/components/Form/Select'
 import { Textarea } from '@/components/Form/TextArea'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -27,19 +28,12 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-100"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
         <form
@@ -218,18 +212,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-100"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
